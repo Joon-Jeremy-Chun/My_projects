c6189aff5beb14bd66b611bff6a0d5894bfe26f3
Joon Jeremy Chun <joonchun1000@gmail.com>
2021-12-08 13:01:44 +0900

    Dec08(01)

--- crypto_market_project/coinpan_crawling.py
+++ crypto_market_project/coinpan_crawling.py
@@ -20,12 +20,15 @@ crawling_count = 10000000
 show_process_log = True
 # show_process_log = False
 
+startP = 1
+endP   = 101
+
 for name, coin in coin_dict.items():
     if show_process_log: print(f"### {name} (전체 코인 중 ")
     if show_process_log: print(f">> 게시글 url 추출 시작")
     post_url_list = list()
-    for page in range(0, 10):
-        if show_process_log: print(f">> [{page} 페이지] 게시글 url 추출 (전체 중 {round(page*100/2, 2)}% 완료)")
+    for page in range(startP, endP):
+        if show_process_log: print(f">> [{page} 페이지] 게시글 url 추출 (전체 중 {round(page*100/(endP-startP), 2)}% 완료)")
         #Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36
         #Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) \Chrome/76.0.3809.146 Whale/2.6.89.9 Safari/537.36'
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
@@ -58,7 +61,7 @@ for name, coin in coin_dict.items():
         date_time_t = target.select_one('div:nth-of-type(1) li:nth-of-type(6)').text.strip()
         date_time.append(date_time_t)
         coin_name.append(name)
-        time.sleep( random.uniform(1,2))
+#        time.sleep( random.uniform(1,2))
    
         
     title_df = pd.DataFrame(title, columns=['title'])
@@ -69,7 +72,7 @@ for name, coin in coin_dict.items():
     date_time_df = date_time_df.convert_dtypes()
     coin_name_df = pd.DataFrame(coin_name,columns=['coin_name'])
     df = pd.concat([title_df,body_df,date_time_df,coin_name_df],axis=1)
-    time.sleep( random.uniform(1,4))
+#    time.sleep( random.uniform(1,4))
 
     print(f">> {name}의 데이터프레임 : ")
     daily_conn = daily_engine.connect()
