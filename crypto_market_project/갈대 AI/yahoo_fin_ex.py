33f9170b7ec37c8e7daca83f68497ed6e435d082
Joon Jeremy Chun <joonchun1000@gmail.com>
2021-12-21 22:31:02 +0900

    Dec21(01)

--- crypto_market_project/갈대 AI/yahoo_fin_ex.py
+++ crypto_market_project/갈대 AI/yahoo_fin_ex.py
@@ -4,4 +4,28 @@ Created on Mon Dec 20 12:12:19 2021
 
 @author: joonc
 """
-
+#%%
+!pip install yahoo_fin
+#%%
+!pip install yahoo_fin --upgrade
+#%%
+import yahoo_fin.stock_info as si
+#%%
+# datetime
+# feedparser
+# ftplib
+# io
+# json
+# pandas
+# requests
+# requests_html
+#%%
+!pip install datetime
+!pip install feedparser
+!pip install ftplib
+!pip install io
+!pip install json
+!pip install pandas
+!pip install requests
+!pip install requests_html
+#%%
