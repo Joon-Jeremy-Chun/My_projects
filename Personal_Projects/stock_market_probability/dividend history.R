5063e216386d29efd90befbbe75689a334815831
Joon Jeremy Chun <joonchun1000@gmail.com>
2023-06-18 17:54:53 +0900

    JUN18-01

---
+++ Personal_Projects/stock_market_probability/dividend history.R
@@ -0,0 +1,20 @@
+library(quantmod)
+
+# Define the ticker symbol for the stock of interest
+ticker_symbol <- "AAPL"  # Replace with your desired stock symbol
+
+# Fetch dividend history using quantmod and Yahoo Finance
+dividend_data <- getDividends(ticker_symbol, src = "yahoo")
+
+# Print dividend history
+print(dividend_data)
+
+# Fetch future dividend plans using Yahoo Finance API
+future_dividends_url <- paste0("https://query1.finance.yahoo.com/v7/finance/quote?&symbols=", ticker_symbol)
+future_dividends_json <- jsonlite::fromJSON(future_dividends_url)
+
+# Extract future dividend information from the JSON response
+future_dividends <- future_dividends_json$quoteSummary$result[[1]]$dividendDate$fmt
+
+# Print future dividend information
+print(future_dividends)
