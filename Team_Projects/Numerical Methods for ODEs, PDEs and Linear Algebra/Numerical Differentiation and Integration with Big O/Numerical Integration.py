a9695cbd6a527b79371f83c5454ca5e7786fd910
Joon Jeremy Chun <joonchun1000@gmail.com>
2024-01-30 13:12:45 -0800

    JAN30-01

---
+++ Team_Projects/Numerical Methods for ODEs, PDEs and Linear Algebra/Numerical Differentiation and Integration with Big O/Numerical Integration.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 30 13:11:09 2024
+
+@author: joonc
+"""
+1
---
+++ Team_Projects/Numerical Methods for ODEs, PDEs and Linear Algebra/Numerical Differentiation and Integration with Big O/p1.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 30 10:07:14 2024
+
+@author: joonc
+"""
+
