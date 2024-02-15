a4d68120e14b28667d84650930304e582e740ec7
Joon Jeremy Chun <joonchun1000@gmail.com>
2024-02-14 22:38:09 -0800

    FEB14-03

--- Mathematical Modeling/Modeling in epidemics/SIR-D-model.py
+++ Mathematical Modeling/Modeling in epidemics/SIR-D-model.py
@@ -1,47 +1,51 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jan 30 15:53:23 2024
+Created on Wed Feb 14 22:26:09 2024
 
 @author: joonc
 """
 
-#ODEs
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
 # SIR model differential equations.
-def deriv(y, t, N, beta, gamma):
-    S, I, R = y
+def deriv(y, t, N, beta, gamma, delta):
+    S, I, R, D = y
     dSdt = -beta * S * I / N
-    dIdt = beta * S * I / N - gamma * I
+    dIdt = beta * S * I / N - gamma * I - delta * I
     dRdt = gamma * I
-    return dSdt, dIdt, dRdt
+    dDdt = delta * I
+    return dSdt, dIdt, dRdt, dDdt
 
 # Initial number of infected and recovered individuals, everyone else is susceptible to infection initially.
+S0 = 999
 I0 = 1
 R0 = 0
-S0 = 999
-N = S0 + I0 + R0
+D0 = 0
+
+N = S0 + I0 + R0 + D0
 
 # Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
-beta = 0.3
-gamma = 0.1
+beta = 0.5
+gamma = 0.2
+delta = 0.02
 
 # A grid of time points (in days)
 t = np.linspace(0, 160, 160)
 
 # Initial conditions vector
-y0 = S0, I0, R0
+y0 = S0, I0, R0, D0
 
 # Integrate the SIR equations over the time grid, t.
-ret = odeint(deriv, y0, t, args=(N, beta, gamma))
-S, I, R = ret.T
+ret = odeint(deriv, y0, t, args=(N, beta, gamma, delta))
+S, I, R, D = ret.T
 
 # Plot the data on three separate curves for S(t), I(t) and R(t)
 plt.plot(t, S, label='Susceptible')
 plt.plot(t, I, label='Infected')
 plt.plot(t, R, label='Recovered')
+plt.plot(t, D, label='Death')
 plt.xlabel('Days')
 plt.ylabel('Population')
 plt.title('SIR Model Example')
