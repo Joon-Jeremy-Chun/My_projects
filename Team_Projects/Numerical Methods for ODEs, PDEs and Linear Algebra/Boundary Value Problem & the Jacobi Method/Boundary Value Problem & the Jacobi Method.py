4934f61b693e33f56bd0be11b5fc648861b780c2
Joon Jeremy Chun <joonchun1000@gmail.com>
2024-04-14 14:12:53 -0700

    APR14-01

--- Team_Projects/Numerical Methods for ODEs, PDEs and Linear Algebra/Boundary Value Problem & the Jacobi Method/Boundary Value Problem & the Jacobi Method.py
+++ Team_Projects/Numerical Methods for ODEs, PDEs and Linear Algebra/Boundary Value Problem & the Jacobi Method/Boundary Value Problem & the Jacobi Method.py
@@ -4,7 +4,6 @@ Created on Thu Apr 11 10:03:15 2024
 
 @author: joonc
 """
-
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -44,6 +43,9 @@ b = np.full((n, ), -h**2 * q)
 # Initial guess u0
 u0 = np.ones(n)
 #u0 = np.array([0,0,1,0,0,1,0,0,1,0,0])
+#u0 = np.array([1,2,3,4,5,6,5,4,3,2,1])
+#u0 = np.array([0,0,-1,0,0,-1,0,0,-1,0,0])
+#u0 = np.array([-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1])
 
 # Error tolerance and maximum number of iterations
 tol = 1e-8
@@ -52,13 +54,36 @@ max_iter = 1000
 # Solve the system using the Jacobi method
 u_approx, num_iter = jacobi(A, b, u0, tol, max_iter)
 
-# Plotting the approximate solution
-x = np.linspace(h, L-h, n)
-plt.plot(x, u_approx)
+# # Plotting the approximate solution
+# x = np.linspace(h, L-h, n)
+# plt.plot(x, u_approx)
+# plt.xlabel('Position along the beam (x)')
+# plt.ylabel('Deflection')
+# plt.title('Beam Deflection Due to Uniform Load')
+# plt.grid(True)
+# plt.show()
+
+# Adding the boundary conditions
+x = np.linspace(0, L, n+2)  # Adjusted to include 0 and L
+u_complete = np.zeros(n+2)
+u_complete[1:-1] = u_approx
+
+# Plotting the approximate solution with boundary conditions
+plt.plot(x, u_complete, label='Deflection')
+plt.scatter([0, L], [0, 0], color='red', label='Boundary Conditions')  # Boundary conditions
 plt.xlabel('Position along the beam (x)')
 plt.ylabel('Deflection')
 plt.title('Beam Deflection Due to Uniform Load')
+plt.legend()
 plt.grid(True)
 plt.show()
 
+# Find the index of the minimum deflection value
+min_deflection_index = np.argmin(u_approx) 
+min_deflection_value = u_approx[min_deflection_index]
+min_deflection_position = x[min_deflection_index] + h # Note: Here we add h because in the code, we put boundary conditions in both ends, so index shift one unite each.
+
+# Print the number of Jacobi iterations, minimum deflection value, and its position
 print(f'Number of Jacobi iterations: {num_iter}')
+print(f'Minimum deflection value: {min_deflection_value}')
+print(f'Position of minimum deflection: {min_deflection_position}')
