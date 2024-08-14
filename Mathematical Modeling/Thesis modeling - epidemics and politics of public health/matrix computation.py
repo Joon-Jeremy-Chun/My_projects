3046af9ad68b31524dbc7a0265dbb24e6b8333be
Joon Jeremy Chun <joonchun1000@gmail.com>
2024-08-14 15:04:06 -0700

    AUG14-01

--- Mathematical Modeling/Thesis modeling - epidemics and politics of public health/matrix computation.py
+++ Mathematical Modeling/Thesis modeling - epidemics and politics of public health/matrix computation.py
@@ -6,16 +6,24 @@ Created on Fri Jul 21 13:19:44 2024
 """
 
 import sympy as sp
-
+#%%
 # Define the symbols
 beta, gamma, mu = sp.symbols('beta gamma mu')
 
-# Constructing the matrix V
+# # Constructing the matrix V
+# V = sp.Matrix([
+#     [-gamma - mu, 0, 0],
+#     [mu, -gamma - mu, 0],
+#     [0, mu, -gamma - mu]
+# ])
+
+a,b,c,d,e = sp.symbols('a b c d e')
 V = sp.Matrix([
-    [-gamma - mu, 0, 0],
-    [mu, -gamma - mu, 0],
-    [0, mu, -gamma - mu]
+    [a, 0, 0],
+    [d, b, 0],
+    [0, e, c]
 ])
+
 
 # Calculating the determinant of the matrix
 determinant = V.det()
@@ -25,7 +33,7 @@ print("Matrix V:")
 sp.pprint(V)
 print("\nDeterminant:")
 sp.pprint(determinant)
-
+#%%
 # Calculating the inverse of the matrix if it is non-singular
 if determinant != 0:
     inverse_matrix = V.inv()
@@ -34,7 +42,7 @@ if determinant != 0:
 else:
     print("\nThe matrix is singular and cannot be inverted.")
     inverse_matrix = None
-
+#%%
 # Define symbols with subscripts
 beta_11, beta_12, beta_13, beta_21, beta_22, beta_23, beta_31, beta_32, beta_33 = sp.symbols('beta_11 beta_12 beta_13 beta_21 beta_22 beta_23 beta_31 beta_32 beta_33')
 N_C, N_A, N_S = sp.symbols('N_C N_A N_C')
